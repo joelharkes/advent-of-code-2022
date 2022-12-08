46ed5e79e617f1ad1237113a4b2b67765005cd01
joelharkes <jlharkes@gmail.com>
2022-12-08 18:15:46 +0100

    small day1 cleanup

--- day1/index.js
+++ day1/index.js
@@ -4,9 +4,9 @@ var text = fs.readFileSync(__dirname + '/data.txt', 'utf-8');
 var elves = text.split("\n\n");
 
 const totalCaloriesPerElf = elves
-    .map(x=> x.split("\n")
-    .map(x=> parseInt(x))
-    .reduce((a,b) => a+b ), 0)
+        .map(x=> x.split("\n")
+        .map(x=> parseInt(x))
+        .reduce((a,b) => a+b ), 0)
 var mostCalories = totalCaloriesPerElf.reduce((a,b) => a < b ? b: a, 0);
 console.log(mostCalories);
 
@@ -15,6 +15,6 @@ var sortedDesc = totalCaloriesPerElf
     .sort((a, b) =>  b - a);
 var sumTop3 =sortedDesc
     .slice(0,3)
-    .reduce((a,b)=> parseInt(a)+parseInt(b),0);
+    .reduce((a,b)=> (a+b),0);
 
 console.log(sumTop3);
